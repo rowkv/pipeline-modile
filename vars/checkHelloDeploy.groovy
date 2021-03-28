f79e7ae2d73a0974d7793d52a0558f0ab60dfc5f
Raman Rohau <raman.rohau@gmail.com>
2021-03-28 22:17:36 +0300

    v.0.7

--- vars/checkHelloDeploy.groovy
+++ vars/checkHelloDeploy.groovy
@@ -1,10 +1,4 @@
 def call(String url, String buildNumber){
-    tx = url.toURL().openConnection().with {
-        setRequestProperty("User-Agent", "Firefox/2.0.0.4")
-        inputStream.with {
-        	def ret = getText( 'UTF-8' )
-        	close()
-        	return ret.contains("Build number: ${buildNumber}")
-        }
-    }
+    def urlText = new URL(url).getText()
+  	return urlText.contains("Build number: ${buildNumber}")
 }
