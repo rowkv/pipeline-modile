39ac714f2f9cf9461c4b8d0962b0755cb0bf6720
Raman Rohau <raman.rohau@gmail.com>
2021-03-28 14:42:45 +0300

    v.0.5

--- vars/pushToDockerReg.groovy
+++ vars/pushToDockerReg.groovy
@@ -1,5 +1,5 @@
-def call(String name, String tag) {
-    docker.withServer('tcp://35.188.219.193:2376') {
+def call(String name, String tag, String server) {
+    docker.withServer(server) {
         docker.withTool('docker'){
 	        docker.withRegistry('https://docker.rrohau.lab.playpit.by', 'nexus-repo') {
 	            	dockerImage = docker.build(name, "./")
